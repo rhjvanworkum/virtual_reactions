32894d5f2d5ebf69ec3b49af1d832683debdf2d7
Ruard van Workum <rhjvanworkum@gmail.com>
2023-03-10 13:36:03 +0100

    scripts

--- scripts/datasets/generation/generate_ff_eas_dataset.py
+++ scripts/datasets/generation/generate_ff_eas_dataset.py
@@ -1,11 +1,11 @@
 import os
-from src.reactions.eas.eas_dataset import Dataset, XtbSimulatedEasDataset
+from src.dataset import Dataset
+from src.reactions.eas.ff_eas_dataset import FFSimulatedEasDataset
 
 BASE_DIR = '/home/rhjvanworkum/virtual_reactions/calculations/'
 BASE_PATH = '/home/rhjvanworkum/virtual_reactions/'
 XTB_PATH = "/home/rhjvanworkum/xtb-6.5.1/bin/xtb"
-N_CPUS = 30
-N_CPUS_CONFORMERS = 4
+N_CPUS = 14
 
 os.environ["BASE_DIR"] = BASE_DIR
 os.environ["BASE_PATH"] = BASE_PATH
@@ -13,11 +13,11 @@ os.environ["XTB_PATH"] = XTB_PATH
 
 
 source_dataset = Dataset(
-    csv_file_path="eas_dataset_2.csv"
+    csv_file_path="eas_dataset.csv"
 )
 
-dataset = XtbSimulatedEasDataset(
-    csv_file_path="xtb_simulated_eas_2_opt_2.csv"
+dataset = FFSimulatedEasDataset(
+    csv_file_path="ff_simulated_eas.csv"
 )
 
 dataset.generate(source_dataset, n_cpus=N_CPUS)
