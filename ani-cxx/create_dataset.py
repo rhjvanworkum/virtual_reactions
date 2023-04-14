82f59226b1813e5cd9fd2d2a2e5452ccead9c189
Ruard van Workum <rhjvanworkum@gmail.com>
2023-04-14 10:03:07 +0200

    added proper splits

--- ani-cxx/create_dataset.py
+++ ani-cxx/create_dataset.py
@@ -57,8 +57,7 @@ if __name__ == "__main__":
     print(f"No. of test Molecules: {len(test_dataset.keys())}")
     print(f"No. of test geometries: {sum([len(test_dataset[k]) for k in test_dataset.keys()])}")
 
-    def get_data(key, idx):
-        atoms_list, property_list = [], []
+    def get_cc_data(key, idx):
         atoms = Atoms(
             numbers=ani_dataset[key][Z_KEY][:],
             positions=ani_dataset[key][R_KEY][:][idx, ...]
@@ -68,31 +67,40 @@ if __name__ == "__main__":
             'energy': np.array([ani_dataset[key][CC_ENERGY_KEY][:][idx]]) / KCAL_MOL_HARTREE,
             'simulation_idx': np.array([0])
         }
-        atoms_list.append(atoms)
-        property_list.append(properties)
+        return atoms, properties
 
+    def get_dft_data(key, idx):
+        atoms = Atoms(
+            numbers=ani_dataset[key][Z_KEY][:],
+            positions=ani_dataset[key][R_KEY][:][idx, ...]
+        )
         # DFT - "simulation"
         properties = {
             'energy': np.array([ani_dataset[key][DFT_ENERGY_KEY][:][idx]]) / KCAL_MOL_HARTREE,
             'simulation_idx': np.array([1])
         }
-        atoms_list.append(atoms)
-        property_list.append(properties)
-        return atoms_list, property_list
+        return atoms, properties
 
     # create ASE DB
     atoms_list = []
     property_list = []
     for key in train_dataset.keys():
         for idx in train_dataset[key]:
-            atoms, properties = get_data(key, idx)
-            atoms_list += atoms
-            property_list += properties
+            atoms, properties = get_cc_data(key, idx)
+            atoms_list.append(atoms)
+            property_list.append(properties)
+
+    for key in train_dataset.keys():
+        for idx in train_dataset[key]:
+            atoms, properties = get_dft_data(key, idx)
+            atoms_list.append(atoms)
+            property_list.append(properties)
+
     for key in test_dataset.keys():
         for idx in test_dataset[key]:
-            atoms, properties = get_data(key, idx)
-            atoms_list += atoms
-            property_list += properties
+            atoms, properties = get_cc_data(key, idx)
+            atoms_list.append(atoms)
+            property_list.append(properties)
 
     dataset = ASEAtomsData.create(
         './10k_dataset.db',
@@ -102,12 +110,46 @@ if __name__ == "__main__":
     dataset.add_systems(property_list, atoms_list)
 
     # get split idxs
-    training_idxs = np.arange(sum([len(train_dataset[k]) for k in train_dataset.keys()]))
-    test_idxs = np.arange(len(training_idxs), len(training_idxs) + sum([len(test_dataset[k]) for k in test_dataset.keys()]))
-    assert len(training_idxs) + len(test_idxs) == sum([len(custom_dataset[k]) for k in custom_dataset.keys()])
+    training_data_idxs = np.arange(sum([len(train_dataset[k]) for k in train_dataset.keys()]))
+    cc_training_data_idxs = training_data_idxs[:int(len(training_data_idxs) // 2)]
+    dft_training_data_idxs = training_data_idxs[int(len(training_data_idxs) // 2):]
+    ood_test_idxs = np.arange(len(training_data_idxs), len(training_data_idxs) + sum([len(test_dataset[k]) for k in test_dataset.keys()]))
+    assert len(cc_training_data_idxs) + len(dft_training_data_idxs) == len(training_data_idxs)
+    assert len(training_data_idxs) + len(ood_test_idxs) == sum([len(custom_dataset[k]) for k in custom_dataset.keys()])
+    assert sum([property_list[idx]['simulation_idx'][0] for idx in cc_training_data_idxs]) == 0
 
-    np.random.shuffle(training_idxs)
-    train_idxs = training_idxs[:int(train_split * len(training_idxs))]
-    val_idxs = training_idxs[int(train_split * len(training_idxs)):]
+    # Split 1) 100% CC + 100% DFT
+    np.random.shuffle(training_data_idxs)
+    train_idxs = training_data_idxs[:int(train_split * len(training_data_idxs))]
+    val_idxs = training_data_idxs[int(train_split * len(training_data_idxs)):]
+    np.savez('split_cc_100_dft_100.npz', train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
 
-    np.savez('split.npz', train_idx=train_idxs, val_idx=val_idxs, test_idx=test_idxs)
+    # Split 2) 100% CC
+    np.random.shuffle(cc_training_data_idxs)
+    train_idxs = cc_training_data_idxs[:int(train_split * len(cc_training_data_idxs))]
+    val_idxs = cc_training_data_idxs[int(train_split * len(cc_training_data_idxs)):]
+    np.savez('split_cc_100.npz', train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
+
+    # Split 3) 10% CC
+    np.random.shuffle(cc_training_data_idxs)
+    idxs = cc_training_data_idxs[:int(0.1 * len(cc_training_data_idxs))]
+    train_idxs = idxs[:int(train_split * len(idxs))]
+    val_idxs = idxs[int(train_split * len(idxs)):]
+    np.savez('split_cc_10.npz', train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
+
+    # Split 4) 100% DFT
+    np.random.shuffle(dft_training_data_idxs)
+    train_idxs = dft_training_data_idxs[:int(train_split * len(dft_training_data_idxs))]
+    val_idxs = dft_training_data_idxs[int(train_split * len(dft_training_data_idxs)):]
+    np.savez('split_dft_100.npz', train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
+
+    # Split 5) 10% CC + 100% DFT
+    np.random.shuffle(dft_training_data_idxs)
+    np.random.shuffle(cc_training_data_idxs)
+    idx = np.concatenate([
+        cc_training_data_idxs[:int(0.1 * len(cc_training_data_idxs))],
+        dft_training_data_idxs
+    ])
+    train_idxs = idxs[:int(train_split * len(idxs))]
+    val_idxs = idxs[int(train_split * len(idxs)):]
+    np.savez('split_cc_10_dft_100.npz', train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
