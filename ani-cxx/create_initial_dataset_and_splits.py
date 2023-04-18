c633546fd89f2100f255169c354c0b5fb5b5a2df
Ruard van Workum <rhjvanworkum@gmail.com>
2023-04-18 15:24:09 +0200

    fixed whole ani-cxx experiment thing

--- ani-cxx/create_initial_dataset_and_splits.py
+++ ani-cxx/create_initial_dataset_and_splits.py
@@ -61,6 +61,8 @@ if __name__ == "__main__":
     percentage_real_data = 0.05
     percentage_simulated_data = 0.10
     train_split, val_split = 0.9, 0.1
+    iid_test_split = 0.05
+    virtual_test_split = 0.05
 
     save_folder = os.path.join('./data/', name)
     if not os.path.exists(save_folder):
@@ -73,9 +75,7 @@ if __name__ == "__main__":
     cc_mol_len = {k: len(v) for k, v in cc_mol_geom_idx.items()}
     sorted_keys = sorted(cc_mol_len, key=cc_mol_len.get)
     test_mols = sorted_keys[-N_test_molecules:]
-    train_mols = sorted_keys[-(N_train_molecules + N_test_molecules):-N_test_molecules]
-
-    
+    train_mols = sorted_keys[-(N_train_molecules + N_test_molecules):-N_test_molecules]    
     
     atoms = []
     properties = []
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 
     # get 100% DFT data
     dft_10_indices = {}
-    idx = 0
+    tot_i = 0
     for mol_idx, mol in enumerate(train_mols):
         idxs = cc_mol_geom_idx[mol]
         np.random.shuffle(idxs)
@@ -105,9 +105,9 @@ if __name__ == "__main__":
             properties.append(property)
             i += 1
 
-        dft_10_indices[mol_idx] = np.arange(idx, idx + i)[:int(percentage_simulated_data * i)]
-        idx += i
-
+        dft_10_indices[mol_idx] = np.arange(tot_i, tot_i + i)[:int(percentage_simulated_data * i)]
+        tot_i += i
+    
     dft_10_indices = {k: [v + n_cc_datapoints for v in values] for k, values in dft_10_indices.items()}
     n_dft_datapoints = len(atoms) - n_cc_datapoints
 
@@ -122,15 +122,33 @@ if __name__ == "__main__":
     n_test_datapoints = len(atoms) - n_dft_datapoints - n_cc_datapoints
 
     assert n_cc_datapoints + n_dft_datapoints + n_test_datapoints == len(atoms)
+    
+    cc_data_idxs = np.arange(n_cc_datapoints)
+    dft_data_idxs = np.arange(n_cc_datapoints, n_cc_datapoints + n_dft_datapoints)
+    dft10_data_idxs = []
+    for k, values in dft_10_indices.items():
+        dft10_data_idxs += values
+    dft10_data_idxs = np.array(dft10_data_idxs)
+    ood_test_idxs = np.arange(len(atoms) - n_test_datapoints, len(atoms))
 
+    np.random.shuffle(cc_data_idxs)
+    np.random.shuffle(dft_data_idxs)
+    np.random.shuffle(dft10_data_idxs)
+    np.random.shuffle(ood_test_idxs)
+
+    assert set(cc_data_idxs).isdisjoint(dft_data_idxs)
+    assert set(cc_data_idxs).isdisjoint(ood_test_idxs)
+    assert set(dft_data_idxs).isdisjoint(ood_test_idxs)
+    assert set(dft10_data_idxs).issubset(dft_data_idxs)
+    assert not set(dft_data_idxs).issubset(dft10_data_idxs)
 
     # save db
-    dataset = ASEAtomsData.create(
-        os.path.join(save_folder, 'cc_dft_dataset.db'),
-        distance_unit='Ang',
-        property_unit_dict={'energy':'kcal/mol', 'simulation_idx': ''}
-    )
-    dataset.add_systems(properties, atoms)
+    # dataset = ASEAtomsData.create(
+    #     os.path.join(save_folder, 'cc_dft_dataset.db'),
+    #     distance_unit='Ang',
+    #     property_unit_dict={'energy':'kcal/mol', 'simulation_idx': ''}
+    # )
+    # dataset.add_systems(properties, atoms)
     
     
     # save splits
@@ -138,31 +156,61 @@ if __name__ == "__main__":
         os.makedirs(os.path.join(save_folder, 'splits/'))
     if not os.path.exists(os.path.join(save_folder, 'splits/mol_splits/')):
         os.makedirs(os.path.join(save_folder, 'splits/mol_splits/'))
-    ood_test_idxs = np.arange(len(atoms) - n_test_datapoints, n_test_datapoints)
 
     # 1) CC_5 split
-    training_idxs = np.arange(n_cc_datapoints)
-    np.random.shuffle(training_idxs)
-    train_idxs = training_idxs[:int(train_split * len(training_idxs))]
-    val_idxs = training_idxs[int(train_split * len(training_idxs)):]
-    np.savez(os.path.join(save_folder, 'splits/cc_5.npz'), train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
+    iid_test_idxs = cc_data_idxs[:int(iid_test_split * len(cc_data_idxs))]
+    leftover_cc_idxs = list(filter(lambda x: x not in iid_test_idxs, cc_data_idxs))
+    train_idxs = leftover_cc_idxs[:int(train_split * len(leftover_cc_idxs))]
+    val_idxs = leftover_cc_idxs[int(train_split * len(leftover_cc_idxs)):]
+    
+    np.savez(
+        os.path.join(save_folder, 'splits/cc_5.npz'), 
+        train_idx=train_idxs, 
+        val_idx=val_idxs, 
+        ood_test_idx=ood_test_idxs,
+        iid_test_idx=iid_test_idxs,
+        virtual_test_idx=[]
+    )
 
     # 2) CC_5_DFT_100 split
-    training_idxs = np.arange(n_cc_datapoints + n_dft_datapoints)
+    virtual_test_idxs = dft_data_idxs[:int(virtual_test_split * len(dft_data_idxs))]
+    leftover_dft_idxs = list(filter(lambda x: x not in virtual_test_idxs, dft_data_idxs))
+    iid_test_idxs = cc_data_idxs[:int(iid_test_split * len(cc_data_idxs))]
+    leftover_cc_idxs = list(filter(lambda x: x not in iid_test_idxs, cc_data_idxs))
+
+    training_idxs = np.array(leftover_dft_idxs + leftover_cc_idxs)
     np.random.shuffle(training_idxs)
     train_idxs = training_idxs[:int(train_split * len(training_idxs))]
     val_idxs = training_idxs[int(train_split * len(training_idxs)):]
-    np.savez(os.path.join(save_folder, 'splits/cc_5_dft_100.npz'), train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
+    
+    np.savez(
+        os.path.join(save_folder, 'splits/cc_5_dft_100.npz'), 
+        train_idx=train_idxs, 
+        val_idx=val_idxs, 
+        ood_test_idx=ood_test_idxs,
+        iid_test_idx=iid_test_idxs,
+        virtual_test_idx=virtual_test_idxs
+    )
 
     # 3) CC_5_DFT_10 split
-    training_idxs = np.arange(n_cc_datapoints).tolist()
-    for k, v in dft_10_indices.items():
-        training_idxs += v
-    training_idxs = np.array(training_idxs)
+    virtual_test_idxs = dft10_data_idxs[:int(virtual_test_split * len(dft10_data_idxs))]
+    leftover_dft_idxs = list(filter(lambda x: x not in virtual_test_idxs, dft_data_idxs))
+    iid_test_idxs = cc_data_idxs[:int(iid_test_split * len(cc_data_idxs))]
+    leftover_cc_idxs = list(filter(lambda x: x not in iid_test_idxs, cc_data_idxs))
+
+    training_idxs = np.array(leftover_dft_idxs + leftover_cc_idxs)
     np.random.shuffle(training_idxs)
     train_idxs = training_idxs[:int(train_split * len(training_idxs))]
     val_idxs = training_idxs[int(train_split * len(training_idxs)):]
-    np.savez(os.path.join(save_folder, 'splits/cc_5_dft_10.npz'), train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
+
+    np.savez(
+        os.path.join(save_folder, 'splits/cc_5_dft_10.npz'), 
+        train_idx=train_idxs, 
+        val_idx=val_idxs, 
+        ood_test_idx=ood_test_idxs,
+        iid_test_idx=iid_test_idxs,
+        virtual_test_idx=virtual_test_idxs
+    )
 
     # 4) MOLECULES split
     for key, value in dft_10_indices.items():
@@ -170,4 +218,11 @@ if __name__ == "__main__":
         np.random.shuffle(training_idxs)
         train_idxs = training_idxs[:int(train_split * len(training_idxs))]
         val_idxs = training_idxs[int(train_split * len(training_idxs)):]
-        np.savez(os.path.join(save_folder, f'splits/mol_splits/mol{key}.npz'), train_idx=train_idxs, val_idx=val_idxs, test_idx=ood_test_idxs)
+        np.savez(
+            os.path.join(save_folder, f'splits/mol_splits/mol{key}.npz'), 
+            train_idx=train_idxs,
+            val_idx=val_idxs, 
+            ood_test_idx=[],
+            iid_test_idx=[],
+            virtual_test_idx=[]
+        )
