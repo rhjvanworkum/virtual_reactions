e571697fb81e60a558fc67351586d1a484a714c2
Ruard van Workum <rhjvanworkum@gmail.com>
2023-05-02 15:20:23 +0200

    MA simulation dataset

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,9 +3,9 @@
 source env.sh
 
 # generation
-# python scripts/datasets/generation/generate_dft_eas_dataset.py
+python scripts/datasets/generation/generate_ma_dataset.py
 
-python scripts/train_chemprop_model.py
+# python scripts/train_chemprop_model.py
 
 # python scripts/generate_xtb_eas_dataset.py
 # python scripts/run_e2_sn2_simulation.py
